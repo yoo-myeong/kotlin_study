fdd83806566ce4a05e0d985a42f626292b54c478
yoo-myeong <yoou201@naver.com>
2022-07-19 23:41:39 +0900

    Exception

---
+++ src/4_Exception.kt
@@ -0,0 +1,30 @@
+﻿fun main() {
+    try{
+        Thread.sleep(1)
+    }catch (e : Exception) {
+        println("Error occur!")
+    } finally {
+        println("finally occur!")
+    }
+
+    // 코틀린에서 try catch는 표현식이기 때문에 값 전달이 가능
+    val a = try {
+        "1234".toInt()
+    }catch (e: Exception) {
+        println("예외 발생!")
+    }
+    println(a)
+
+//    throw Exception("예외 발생")
+
+    val b : String? = null
+    val c = b?: failFast("b is null")
+
+    println(c?.length)
+
+}
+
+
+fun failFast(msg : String):Nothing {
+    throw IllegalArgumentException(msg)
+}
