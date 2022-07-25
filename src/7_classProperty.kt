182d825f4660af00878cd956e0b756f303d35ac7
yoo-myeong <yoou201@naver.com>
2022-07-25 23:19:23 +0900

    클래스

---
+++ src/7_classProperty.kt
@@ -0,0 +1,32 @@
+﻿class Coffee(
+    var name : String = "",
+    var price : Int = 0,
+    val hotOrIce : String,
+    // var로 선언하면 getter, setter 가 만들어지고
+    // val은 getter만 만들어짐
+) {
+    val brand : String
+        get() {
+            return "스타벅스"
+        }
+
+    var quantity : Int = 0
+        set(value) {
+            if(value > 0) {
+                field = value
+            }
+        }
+}
+
+class EmptyClass
+
+fun main() {
+    val coffee = Coffee(hotOrIce= "hot")
+    coffee.name = "아이스아메리카노"
+    coffee.price = 2000
+
+    coffee.quantity = -1
+
+    println(coffee.brand)
+    println(coffee.quantity)
+}
