1b3ec11938332093a5427d7c764a4b7e911d7975
yoo-myeong <yoou201@naver.com>
2022-08-03 08:39:12 +0900

    collection

--- src/1_Collection.kt
+++ src/1_Collection.kt
@@ -18,7 +18,7 @@ fun main() {
     }
 
     // immutable set
-    val numberSEt = setof(1,2,3,4)
+    val numberSEt = setOf<Int>(1,2,3,4)
 
     // mutable set
     val mutableSet = mutableSetOf<Int>().apply{
@@ -44,10 +44,34 @@ fun main() {
     }
 
     // linked list
-
     val linkedList = LinkedList<Int>().apply {
-        addFirst((3))
+        addFirst(3)
         add(2)
         addLast(1)
     }
+
+    // arrayList
+    val arrayList = ArrayList<Int>().apply {
+        add(1)
+        add(2)
+        add(3)
+    }
+
+    val iterator = currencyList.iterator()
+    while (iterator.hasNext()){
+        println(iterator.next())
+    }
+
+    for (currency in currencyList) {
+        println(currency)
+    }
+
+    currencyList.forEach {
+        println(it)
+    }
+
+    // map
+    val lowerList = listOf("a", "b", "c")
+    val upperList = lowerList.map { it.uppercase() }
+    val filteredList = lowerList.filter {it=="a" || it == "c"}
 }
