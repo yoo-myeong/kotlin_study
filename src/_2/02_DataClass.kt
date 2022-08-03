ab3c135cee8cc2b2fe8a39d1fea609274e3eed3b
yoo-myeong <yoou201@naver.com>
2022-08-03 23:05:08 +0900

    data class

---
+++ src/_2/02_DataClass.kt
@@ -0,0 +1,29 @@
+﻿package _2
+
+class Person(val name:String, val age:Int)
+data class PersonData(val name:String, val age:Int)
+
+fun main() {
+    val person1 = Person(name = "tony", age = 12)
+    val person2 = Person(name = "tony", age = 12)
+
+    val person3 = PersonData(name = "tony", age = 12)
+    val person4 = PersonData(name = "tony", age = 12)
+
+    println(person1 == person2)
+    println(person3 == person4)
+
+    val set = hashSetOf(person3)
+    println(set.contains(person4))
+
+    println(person1.toString())
+    println(person3.toString())
+
+    val person5 = person3.copy(name = "stranger")
+    println(person5)
+
+    println("이름  = ${person5.component1()}, 나이 = ${person5.component2()}")
+
+    val (name, age) = person5
+    println("이름  = $name, 나이 = $age")
+}
