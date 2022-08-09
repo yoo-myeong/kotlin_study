e5c873858aa0d0d8fda2b5808d463365fa074765
yoo-myeong <yoou201@naver.com>
2022-08-10 00:29:54 +0900

    지연초기화

---
+++ src/_2/7_LazyInit.kt
@@ -0,0 +1,25 @@
+﻿package _2
+
+class HelloBot {
+
+    val greeting: String by lazy(LazyThreadSafetyMode.PUBLICATION) {
+        getHello()
+    }
+
+    fun sayHello() = println(greeting)
+}
+
+fun getHello() = "안녕하세요"
+
+fun main() {
+    val helloBot = HelloBot()
+
+    // ...
+    // ...
+    for (i in 1..5) {
+        Thread {
+            helloBot.sayHello()
+        }.start()
+    }
+
+}
