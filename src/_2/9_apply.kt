2ffa4040eff84219bfbfa04a25ae109c42c4ac61
yoo-myeong <yoou201@naver.com>
2022-08-10 21:41:42 +0900

    스코프 함수

---
+++ src/_2/9_apply.kt
@@ -0,0 +1,13 @@
+﻿package _2
+
+fun main() {
+
+    DatabaseClient().apply {
+        url = "localhost:3306"
+        username = "mysql"
+        password = "1234"
+    }.connect()
+        .run { println(this) }
+
+
+}
---
+++ src/_2/9_with.kt
@@ -0,0 +1,11 @@
+﻿package _2
+
+fun main() {
+
+    val str = "안녕하세요"
+
+    val length: Int = with(str) {
+        length
+    }
+    println(length)
+}
